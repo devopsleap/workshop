d45536023f9363ac371d45a663180155d7b52b82
Dhiren Shah <dharnendra.shah@crest.in>
2017-06-02 23:17:27 +0530

    Created HelloWorld Project

---
+++ HelloWorld/src/com/devopsleap/comman/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.devopsleap.comman;
+
+/**
+ * @author shah02
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HelloWorld.main()");
+		System.out.println("Welcome to Workshop");
+
+	}
+
+}
